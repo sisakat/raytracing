02ccd7573637c9cde0049897a4ac99a81fb00c19
Stefan Isak <isakhandy@gmail.com>
2022-04-09 07:40:34 +0200

    first ppm image

--- RayTracingInOneWeekend/src/main.cpp
+++ RayTracingInOneWeekend/src/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <spdlog/spdlog.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     const int image_width = 256;
     const int image_height = 256;
 
-    spdlog::info("Test1");
+    std::cout << fmt::format("P3\n{} {}\n255", image_width, image_height)
+              << std::endl;
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << fmt::format("{} {} {}", ir, ig, ib) << std::endl;
+        }
+    }
 }

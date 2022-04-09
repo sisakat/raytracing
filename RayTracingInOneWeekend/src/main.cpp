535f8b925486f772697f70676f636fae7a478434
Stefan Isak <isakhandy@gmail.com>
2022-04-09 07:24:33 +0200

    basic setup

---
+++ RayTracingInOneWeekend/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+int main(int argc, char* argv[]) {
+    const int image_width = 256;
+    const int image_height = 256;
+
+    spdlog::info("Test1");
+}

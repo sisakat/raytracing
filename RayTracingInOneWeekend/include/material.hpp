e4e8b6f5ead36539a08003a1f46c40e3b689baab
Stefan Isak <isakhandy@gmail.com>
2022-04-10 07:13:54 +0200

    hollow glass sphere

--- RayTracingInOneWeekend/include/material.hpp
+++ RayTracingInOneWeekend/include/material.hpp
@@ -71,11 +71,33 @@ public:
         double refraction_ratio = rec.front_face ? (1.0 / m_index_of_refraction)
                                                  : m_index_of_refraction;
         Vec3 unit_direction = r_in.direction().normalized();
-        Vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
-        scattered = Ray(rec.p, refracted);
+        double cos_theta = fmin((-unit_direction).dot(rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        Vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > randomDouble())
+        {
+            direction = reflect(unit_direction, rec.normal);
+        }
+        else
+        {
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+        }
+
+        scattered = Ray(rec.p, direction);
         return true;
     }
 
 private:
     double m_index_of_refraction;
+
+    static double reflectance(double cosine, double ref_idx)
+    {
+        // use Schlick's approximation for reflectance
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
 };
--- RayTracingInOneWeekend/src/main.cpp
+++ RayTracingInOneWeekend/src/main.cpp
@@ -44,15 +44,16 @@ int main(int argc, char* argv[])
     // World
     HittableList world;
     auto material_ground = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
-    auto material_center = std::make_shared<Dialectric>(1.5);
+    auto material_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
     auto material_left = std::make_shared<Dialectric>(1.5);
-    auto material_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), .2);
+    auto material_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.0);
 
     world.add(
         std::make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(
         std::make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), -0.4, material_left));
     world.add(std::make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
